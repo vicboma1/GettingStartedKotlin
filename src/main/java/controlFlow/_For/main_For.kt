336a9b76194ffb5632d0490fb3bdea7994b62a20
Victor Bolinches <vicboma1@users.noreply.github.com>
2015-10-28 22:55:12 +0100

    update folder Control Flow

---
+++ src/main/java/controlFlow/_For/main_For.kt
@@ -0,0 +1,29 @@
+package controlFlow._For
+
+/**
+ * Created by vicboma on 28/10/15.
+ */
+fun main(args: Array<String>) {
+
+    var arrayAny = arrayOf(12,2.3,45F,"Soy una String",true, null)
+    anIterator(arrayAny)
+    withBodyBlock(arrayAny)
+    withIndices(arrayAny)
+}
+
+private fun withIndices(arrayAny: Array<Any?>) {
+    for (i in arrayAny.indices)
+        println(arrayAny[i])
+}
+
+private fun anIterator(arrayAny: Array<Any?>) {
+    for (any in arrayAny)
+        println(any)
+}
+
+private fun withBodyBlock(arrayAny: Array<Any?>) {
+    for (any: Any? in arrayAny) {
+        print(any)
+        print("\n")
+    }
+}
---
+++ src/main/java/controlFlow/_while/mainWhile.kt
@@ -0,0 +1,38 @@
+package controlFlow._while
+
+/**
+ * Created by vicboma on 28/10/15.
+ */
+fun main(args: Array<String>) {
+
+    var arrayAny = arrayOf(1,2,3,4,5)
+    withIterator(arrayAny)
+
+    var arraySafety = arrayOf<Any?>(1,2,3,4,5,null)
+    doWhile(arraySafety)
+
+    classic()
+}
+
+private fun classic() {
+    var i = 5
+    while (0 <= i) {
+        println(i--)
+    }
+}
+
+private fun doWhile(arraySafety: Array<Any?>) {
+    val iterator = arraySafety.iterator()
+    do {
+        val y = iterator.next()
+        println(y)
+    } while (y != null) // y is visible here!
+}
+
+private fun withIterator(arrayAny: Array<Int>) {
+    val iterator = arrayAny.iterator()
+    while (iterator.hasNext()) {
+        val next = iterator.next()
+        println(next)
+    }
+}
